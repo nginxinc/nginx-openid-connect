94861f936a98bd8d1c3f7328ebb7740e9c7e38ed
Liam Crilly <liam.crilly@nginx.com>
2018-03-28 18:26:29 +0100

    Separate conf files

--- openid_connect.js
+++ openid_connect.js
@@ -1,6 +1,10 @@
 /*
- * OpenID Connect functions
+ * nginScript functions for providing OpenID Connect authorization
+ * code flow with NGINX Plus.
+ *
+ * Copyright (C) 2018 Nginx, Inc.
  */
+
 var auth_token = "";
 
 function oidc_codeExchange(req, res) {
@@ -19,27 +23,50 @@ function oidc_codeExchange(req, res) {
 
     // Pass the authorization code to the /_token location so that it can be
     // proxied to the IdP in exchange for a JWT
+    var isValid = 0;
     req.subrequest("/_token", {"args":"code=" + req.variables.arg_code,"method":"POST"},
         function(reply) {
             if (reply.status == 200) {
                 try {
                     var tokenset = JSON.parse(reply.body);
-                    req.log("OIDC received tokenset, accessing " + req.variables.oidc_token_type);
-                    if (tokenset[req.variables.oidc_token_type]) {
-                        res.status = 302;
-                        auth_token = tokenset[req.variables.oidc_token_type]; // Export as NGINX variable
-                        res.headers.Location = req.variables.cookie_auth_redir;
-                    } else {
-                        res.log("OIDC authorization code sent but no token received. " + tokenset.error + " " + tokenset.error_description);
-                        res.status = 500;
-                    }
+                    // Send the ID Token to auth_jwt location for validation
+                    req.subrequest("/_id_token_validation", "token=" + tokenset.id_token + "&nonce=" + req.variables.cookie_auth_nonce,
+                        function(reply) {
+                            if (reply.status == 200) {
+                                var iat = Math.floor(Number(reply.body));
+                                if (String(iat) == reply.body && iat > 0) {
+                                    if (tokenset[req.variables.oidc_token_type]) {
+                                        req.log("OIDC success, sending " + req.variables.oidc_token_type);
+                                        res.status = 302;
+                                        auth_token = tokenset[req.variables.oidc_token_type]; // Export as NGINX variable
+                                        res.headers.Location = req.variables.cookie_auth_redir;
+                                    } else {
+                                        req.log("OIDC authorization code sent but no token received. " + tokenset.error + " " + tokenset.error_description);
+                                        res.status = 500;
+                                    }
+                                } else {
+                                    req.log("OIDC ID Token claim 'iat' is not a valid date");
+                                    res.status = 500;
+                                }
+                            } else {
+                                req.log("OIDC ID Token Validation failure " + reply.status + ", invalid or missing " + reply.body);
+                                res.status = 500;
+                            }
+                            res.sendHeader();
+                            res.finish();
+                        }
+                    );
                 } catch (e) { 
                     req.log("OIDC authorization code sent but response is not JSON. " + reply.body);
                     res.status = 502;
+                    res.sendHeader();
+                    res.finish();
                 }
             } else if (reply.status == 504) {
                 req.log("OIDC timeout connecting to IdP when sending authorization code");
                 res.status = 504;
+                res.sendHeader();
+                res.finish();
             } else {
                 try {
                     var errorset = JSON.parse(reply.body);
@@ -52,9 +79,9 @@ function oidc_codeExchange(req, res) {
                     req.log("OIDC unexpected response from IdP when sending authorization code (HTTP " + reply.status + "). " + reply.body);
                 }
                 res.status = 503;
+                res.sendHeader();
+                res.finish();
             }
-            res.sendHeader();
-            res.finish();
         }
     );
 }
@@ -63,14 +90,18 @@ function get_auth_token(req,res) {
     return auth_token;
 }
 
-function hash_RequestId(req) {
+function hashRequestId(req) {
     var c = require('crypto');
     var h = c.createHash('sha256').update(req.variables.request_id);
     return(h.digest('hex'));
 }
 
-function hash_NonceCookie(req) {
-    var c = require('crypto');
-    var h = c.createHash('sha256').update(req.variables.cookie_auth_nonce);
-    return(h.digest('hex'));
+function hashClientNonce(req) {
+    if (req.variables.arg_nonce.length) {
+        var c = require('crypto');
+        var h = c.createHash('sha256').update(req.variables.arg_nonce);
+        return(h.digest('hex'));
+    } else {
+        return "";
+    }
 }
