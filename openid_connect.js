5adeb5f0f1a4ad6ed96eb50c4d0aa3413e118516
Liam Crilly <liam.crilly@nginx.com>
2018-04-02 23:18:20 +0100

    Moved ID token validation to njs

--- openid_connect.js
+++ openid_connect.js
@@ -25,27 +25,26 @@ function oidc_codeExchange(req, res) {
             if (reply.status == 200) {
                 try {
                     var tokenset = JSON.parse(reply.body);
-                    // Send the ID Token to auth_jwt location for validation
-                    req.subrequest("/_id_token_validation", "token=" + tokenset.id_token + "&nonce=" + req.variables.cookie_auth_nonce,
-                        function(reply) {
-                            if (reply.status == 204) {
-                                if (tokenset[req.variables.oidc_token_type]) {
+                    if (tokenset[req.variables.oidc_token_type]) {
+                        // Send the ID Token to auth_jwt location for validation
+                        req.subrequest("/_id_token_validation", "token=" + tokenset.id_token,
+                            function(reply) {
+                                if (reply.status == 204) {
                                     req.log("OIDC success, sending " + req.variables.oidc_token_type);
                                     auth_token = tokenset[req.variables.oidc_token_type]; // Export as NGINX variable
                                     res.return(302, req.variables.cookie_auth_redir);
                                 } else {
-                                    req.error("OIDC received id_token but not " + req.variables.oidc_token_type + " received");
-                                    if (tokenset.error) {
-                                        req.error("OIDC " + tokenset.error + " " + tokenset.error_description);
-                                    }
                                     res.return(500);
                                 }
-                            } else {
-                                req.error("OIDC ID Token Validation failure " + reply.status + ", invalid or missing " + reply.body);
-                                res.return(500);
                             }
+                        );
+                    } else {
+                        req.error("OIDC received id_token but not " + req.variables.oidc_token_type + " received");
+                        if (tokenset.error) {
+                            req.error("OIDC " + tokenset.error + " " + tokenset.error_description);
                         }
-                    );
+                        res.return(500);
+                    }
                 } catch (e) {
                     req.error("OIDC authorization code sent but token response is not JSON. " + reply.body);
                     res.return(502);
@@ -80,16 +79,48 @@ function hashRequestId(req) {
     return(h.digest('base64'));
 }
 
-function hashClientNonce(req) {
-    if (req.variables.arg_nonce.length) {
-        var c = require('crypto');
-        var h = c.createHmac('sha256', req.variables.oidc_hmac_key).update(req.variables.arg_nonce);
-        return(h.digest('base64'));
-    } else {
-        return "";
-    }
-}
-
 function validateIdToken(req,res) {
-    res.return(204);
+    // Check mandatory claims
+    var required_claims = ["aud", "iat", "iss", "sub"];
+    var missing_claims = [];
+    for (var i in required_claims) {
+        if (req.variables["jwt_claim_" + required_claims[i]].length == 0 ) {
+            missing_claims.push(required_claims[i]);
+        }
+    }
+    if (missing_claims.length) {
+        req.error("OIDC ID Token validation error: missing claim(s) " + missing_claims.join(" "));
+        res.return(403);
+    }
+
+    // Check iat is a number
+    var iat = Math.floor(Number(req.variables.jwt_claim_iat));
+    if (String(iat) != req.variables.jwt_claim_iat || iat < 1) {
+        req.error("OIDC ID Token validation error: iat claim is not a valid number");
+        res.return(403);
+    }
+
+    // TODO: Check iss is substring of IdP root URL
+
+    // Audience matching
+    if (req.variables.jwt_claim_aud != req.variables.oidc_client) {
+        req.error("OIDC ID Token validation error: aud claim does not match $oidc_client");
+        res.return(403);
+    }
+
+    // If we receive a nonce in the ID Token then we will use the auth_nonce cookie
+    // to check that the JWT can be validated as being directly related to the
+    // original request by this client. This mitigates against token replay attacks.
+    var client_nonce_hash = "";
+    if (req.variables.cookie_auth_nonce) {
+        var c = require('crypto');
+        var h = c.createHmac('sha256', req.variables.oidc_hmac_key).update(req.variables.cookie_auth_nonce);
+        client_nonce_hash = h.digest('base64');
+    }
+    if (req.variables.jwt_claim_nonce != client_nonce_hash) {
+        req.error("OIDC ID Token validation error: nonce mismatch");
+        res.return(403);
+    }
+
+    res.return(204); // ID Token validation successful
 }
